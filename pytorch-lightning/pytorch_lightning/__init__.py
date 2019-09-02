e9c74953e6feb21600dba2ee892164ec58315acd
vatsalg29 <vatsalgoel.98@gmail.com>
2019-09-03 01:44:24 +0530

    reorganize

--- pytorch-lightning/pytorch_lightning/__init__.py
+++
@@ -1,9 +0,0 @@
-from .models.trainer import Trainer
-from .root_module.root_module import LightningModule
-from .root_module.decorators import data_loader
-
-__all__ = [
-    'Trainer',
-    'LightningModule',
-    'data_loader',
-]
--- pytorch-lightning/pytorch_lightning/callbacks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .pt_callbacks import EarlyStopping, ModelCheckpoint
-
-__all__ = [
-    'EarlyStopping',
-    'ModelCheckpoint',
-]
